ff6e2116a2f4ee2c295b1a8670a528a2b3b3d0d0
Haseeb Akbar <haseeb2000@hotmail.com>
2018-03-24 18:19:40 +0000

    worked more on styling.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
 const Header = () => {
-  return(
-    <header><a href="">Crypto Companion</a></header>
+  return (
+    <header>
+      <h3>Crypto Companion </h3>
+      <h3 className = 'market-cap'>Market Cap: $340,642,147,019</h3>
+    </header>
   )
 }
 
