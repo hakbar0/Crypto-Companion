1db3c925c703f39aab598f0cae36bad8e923ab58
Haseeb Akbar <haseeb2000@hotmail.com>
2018-03-25 14:22:28 +0100

    Made a dynamic homepage layout.

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Homepage from './Homepage';
-import '../styles/App.css';
+
 
 
 class App extends Component {
