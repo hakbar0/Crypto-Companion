123f4a36690b8b54825ce6fd52e12ecae8479575
Haseeb Akbar <haseeb2000@hotmail.com>
2018-03-25 15:07:01 +0100

    Able to retrieve data for coins.

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,42 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Homepage from './Homepage';
+import cc from 'cryptocompare';
+import '../styles/Homepage.css';
 
 
 
 class App extends Component {
+
+  state = {
+    coins: ''
+  }
+
+  componentDidMount() {
+    this.fetchCoins();
+  }
+
+  fetchCoins = () => (
+    cc.coinList().then(coinList => {
+      this.setState({ coins: Object.keys(coinList.Data) })
+    })
+  )
+
   render() {
     return (
-      <Homepage />
-    );
+      <div id="wrap">
+        <div className="nested-items">
+          <div className="title">
+            <h3>CC</h3>
+          </div>
+          <div className="side-nav">
+        
+          
+          </div>
+        </div>
+        <div className="main"></div>
+        <div className="footer"></div>
+      </div>
+    )
   }
 }
 
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import cc from 'cryptocompare';
 import '../styles/Homepage.css';
+
+
 
 const Homepage = () => {
   return (
     <div id="wrap">
       <div className="nested-items">
-
         <div className="title">
-        <h3>CC</h3>
+          <h3>CC</h3>
         </div>
-
-        <div className="side-nav"></div>
+        <div className="side-nav">{
+          cc.coinList().then(coinList => {
+            console.log(Object.keys(coinList.Data))
+          })}</div>
       </div>
       <div className="main"></div>
       <div className="footer"></div>
