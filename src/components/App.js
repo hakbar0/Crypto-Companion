542f4ed984c7e5e6403bb585f392dbfe314cd659
Haseeb Akbar <haseeb2000@hotmail.com>
2018-03-25 14:05:47 +0100

    Main layout sorted. Need to make sure it scales nicely.

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,13 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Footer from './Footer';
-import Main from './Main';
-import Header from './Header';
+import Homepage from './Homepage';
 import '../styles/App.css';
 
 
 class App extends Component {
   render() {
     return (
-      <div className = "Homepage">
-        <Header />
-        <Main />
-        <Footer />
-      </div>
+      <Homepage />
     );
   }
 }
---
+++ src/components/Homepage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Homepage = () => {
+  return (
+    <div id="wrap">
+      <div className="nested-items">
+        <div className="title"></div>
+        <div className="side-nav"></div>
+      </div>
+      <div className="main"></div>
+      <div className="footer"></div>
+    </div>
+  )
+}
+
+export default Homepage;
